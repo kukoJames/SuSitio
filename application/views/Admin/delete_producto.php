aac1e6c364ec0d03e7e6cc5d3a16a94569dcb14d
Kuko <refugio@canteradigital.mx>
2017-12-09 12:22:22 -0600

    FIX: Correccion

--- application/views/Admin/delete_producto.php
+++ application/views/Admin/delete_producto.php
@@ -4,7 +4,7 @@
 		<div class="row col-sm-12">
 			<input type="hidden" name="id_producto" id="id_producto" value="<?php echo $producto->id_producto ?>">
 			<p style="font-size: 25px; text-align: center;">
-				¿Deesea eliminar <strong><?php echo $producto->nombre ?></strong></p>
+				¿Desea eliminar <strong><?php echo $producto->nombre ?></strong></p>
 			<p style="font-size: 25px; text-align: center;">
 				con un precio de <strong><?php echo number_format($producto->precio,2,'.',',') ?></strong> ?.</p>
 		</div>
